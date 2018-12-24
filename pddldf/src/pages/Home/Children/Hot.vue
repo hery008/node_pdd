fcdcb2da0d0e3b59c67ebfffb29cbdad4e2e87e0
ldf <363934338@qq.com>
2018-12-24 22:44:49 +0800

    轮播图实现

--- pddldf/src/pages/Home/Children/Hot.vue
+++ pddldf/src/pages/Home/Children/Hot.vue
@@ -1,21 +1,47 @@
 <template>
   <div class="hot">
-    我是热门板块
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide"><img src="../imgs/rowing/s1.png" width="100%" alt=""></div>
+        <div class="swiper-slide"><img src="../imgs/rowing/s2.png" width="100%" alt=""></div>
+        <div class="swiper-slide"><img src="../imgs/rowing/s3.png" width="100%" alt=""></div>
+        <div class="swiper-slide"><img src="../imgs/rowing/s4.png" width="100%" alt=""></div> 
+        <div class="swiper-slide"><img src="../imgs/rowing/s5.png" width="100%" alt=""></div>
+        <div class="swiper-slide"><img src="../imgs/rowing/s6.png" width="100%" alt=""></div>
+        <div class="swiper-slide"><img src="../imgs/rowing/s7.png" width="100%" alt=""></div>
+      </div>
+        <!-- 如果需要分页器 -->
+        <div class="swiper-pagination"></div>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "Hot"
+import Swiper from "swiper"
+import "swiper/dist/css/swiper.min.css"
+
+export default {
+  name: "Hot",
+  mounted () {
+    //创建swiper实例
+    new Swiper('.swiper-container', {
+      autoplay:true,
+      loop: true, // 循环模式选项
+
+      // 如果需要分页器
+      pagination: {
+        el: '.swiper-pagination',
+      }
+    })
   }
+}  
 </script>
 
 <style scoped lang="stylus" ref="stylesheet/stylus">
-  .hot
-    width 100%
-    height 100%
-    background purple
-    display flex
-    justify-content center
-    align-items center
+.hot {
+  width: 100%;
+  height: 100%;
+  background: purple;
+  padding-top: 46px;
+}
 </style>
--- pddldf/src/pages/Home/Home.vue
+++ pddldf/src/pages/Home/Home.vue
@@ -1,52 +1,60 @@
 <template>
-<div class="home">
-  <ly-tab
-    v-model="selectedId"
-    :items="items"
-    :options="options"
-    @change="handleChange"
-  >
-  </ly-tab>
+  <div class="home">
+    <ly-tab v-model="selectedId"
+            :items="items"
+            :options="options"
+            @change="handleChange"
+            class="fix">
+    </ly-tab>
 
-  <router-view></router-view>
-</div>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Home",
-      data(){
-          return{
-            selectedId:0,//默认选中的Id
-            items:[
-              {label: '热门'},
-              {label: '服饰'},
-              {label: '鞋包'},
-              {label: '母婴'},
-              {label: '百货'},
-              {label: '食品'},
-              {label: '内衣'},
-              {label: '男装'},
-              {label: '电器'}
-            ],
-            options:{
-              activeColor:'#e9232c'//选中的颜色
-            },
-            // 二级路由路径
-            subRouteUrl: ['/home/hot', '/home/dress', '/home/box', '/home/mbaby', '/home/general', '/home/food', '/home/shirt', '/home/man', '/home/ele']
-          }
+
+export default {
+  name: "Home",
+  data () {
+    return {
+      selectedId: 0,//默认选中的Id
+      items: [
+        { label: '热门' },
+        { label: '服饰' },
+        { label: '鞋包' },
+        { label: '母婴' },
+        { label: '百货' },
+        { label: '食品' },
+        { label: '内衣' },
+        { label: '男装' },
+        { label: '电器' }
+      ],
+      options: {
+        activeColor: '#e9232c'//选中的颜色
       },
-      methods:{
-        handleChange(item,index){
-          this.$router.replace(this.subRouteUrl[index])
-        }
-      }
+      // 二级路由路径
+      subRouteUrl: ['/home/hot', '/home/dress', '/home/box', '/home/mbaby', '/home/general', '/home/food', '/home/shirt', '/home/man', '/home/ele']
     }
+  },
+  methods: {
+    handleChange (item, index) {
+      this.$router.replace(this.subRouteUrl[index])
+    }
+  }
+}
 </script>
 
 <style scoped lang="stylus" ref="stylesheet/stylus">
-.home
-  width: 100%
-  height:100%;
-  background-color: lightgreen
+.home {
+  width: 100%;
+  height: 100%;
+  background-color: lightgreen;
+
+  .fix {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 998;
+  }
+}
 </style>
